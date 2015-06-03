0f1652e6d41f54b8d0c12baf5cab3e7aa9105bc3
Pigmoth69 <pigmoth@gmail.com>
2015-06-03 17:11:45 +0100

    com alterações
    cenas

--- core/src/com/mygdx/SkeetPro/gamestate/SaveClass.java
+++ core/src/com/mygdx/SkeetPro/gamestate/SaveClass.java
@@ -12,12 +12,12 @@ public class SaveClass implements Serializable{
 	
 	
 	SaveClass(Player p1){
-		nome = p1.getName();
-		score = p1.getScore();
+		//nome = p1.getName();
+		//score = p1.getScore();
 	}
 	
 	public void LoadSave(){
-       System.out.println("Score guardado: " + nome + " - " + score);
+      // System.out.println("Score guardado: " + nome + " - " + score);
 	}
 	
 }
