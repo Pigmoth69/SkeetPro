7fe4353bb5f024ab7b71e0b641f06e4ce2af37d8
Pigmoth69 <pigmoth@gmail.com>
2015-06-03 11:02:02 +0100

    estoiu a ficar fdd belho
    belho

---
+++ desktop/src/com/mygdx/SkeetPro/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.SkeetPro.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.SkeetPro.SkeetPro;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SkeetPro(), config);
+	}
+}
---
+++ ios/src/com/mygdx/SkeetPro/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.mygdx.SkeetPro;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.mygdx.SkeetPro.SkeetPro;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new SkeetPro(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
